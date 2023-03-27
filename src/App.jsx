9fe7dca468435d309bbb1d4599ae7c3e4edfe07d
Artem Chernii <104429440+artemcherniiosf@users.noreply.github.com>
2023-03-27 21:25:14 +0100

    feat: module system

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import Button from './components/Button';
+
+export default function App() {
+    const clickHandler = (e) => {
+        console.log(e, 'click handler');
+    };
+    const [inputValue, setInputValue] = useState('');
+    const onChangeHandler = (event) => {
+        console.log(event.target.value);
+        setInputValue(event.target.value);
+    };
+    const inputType = 'number';
+    const maxValue = 5;
+    return (
+        <>
+            <input
+                type="text"
+                placeholder="Type something here"
+                onChange={onChangeHandler}
+                value={inputValue}
+            />
+            <input type="text" />
+            <input
+                type={inputType}
+                max={maxValue}
+                spellCheck
+                list={[1, 2, 3]}
+                style={{ color: 'tomato', border: '2vmin solid red' }}
+            />
+            <h1>{inputValue}</h1>
+            <Button text={'New Button'} onClickHandler={clickHandler}></Button>
+        </>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,18 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Button from './components/Button';
+import App from './App';
 
-function App() {
-    const clickHandler = (e) => {
-        console.log(e);
-        console.log('click handler');
-    };
-    return (
-        <>
-            <h1>Hello JSX</h1>
-            <Button text={'New Button'} onClickHandler={clickHandler}></Button>
-        </>
-    );
-}
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 root.render(<App />);
